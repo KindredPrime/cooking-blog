824b082e0ab336b3c01e816be4aad70a45c07c24
Ken Wright <kenneth.gordon.wright@gmail.com>
2020-12-28 15:25:38 -0500

    Create Home component

---
+++ src/Home/index.test.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import Home from './index';
+
+describe('Home Component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Home />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
---
+++ src/PostsList/index.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class PostsList extends Component {
+  render() {
+    return (
+      <div className="PostsList">
+
+      </div>
+    );
+  }
+}
+
+export default PostsList;
