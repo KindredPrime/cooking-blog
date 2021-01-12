f3042f930b785f02f979907126c8bf1d1866a364
Ken Wright <kenneth.gordon.wright@gmail.com>
2021-01-12 16:21:57 -0500

    Create ScrollToTop component for Router

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import ScrollToTop from './ScrollToTop/index';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
